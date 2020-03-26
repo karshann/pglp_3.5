30188455e63feae42aa31c4cb9aa8a2066bf84dc
karshann <karshan951@gmail.com>
2020-03-26 15:49:14 +0100

    creation des classe

---
+++ src/main/java/org/example/Log.java
@@ -0,0 +1,6 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Log implements Message {
+}
---
+++ src/main/java/org/example/Message.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface Message {
+    void affichermessage(String S);
+}
