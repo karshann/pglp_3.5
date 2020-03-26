821d1d570e8bd003437dd9e22ec71ceb37702879
karshann <karshan951@gmail.com>
2020-03-26 15:50:48 +0100

    implementation de la classe uneClasseMetier

--- src/main/java/org/example/UneClasseMetier.java
+++ src/main/java/org/example/UneClasseMetier.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class UneClasseMetier {
+    Log l;
+    public void uneMethodeMetier() {
+        l.affichermessage(": Début de,uneMethodeMetier " );
+        // Traitement métiers
 
+        l.affichermessage( ": Fin de uneMethodeMetier " );
+    }
 }
 
