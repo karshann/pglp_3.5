5a82bd0116ce0f9a17e9a72f02db6adb29adab46
karshann <karshan951@gmail.com>
2020-03-26 15:50:03 +0100

    implemantation de la classe log

--- src/main/java/org/example/Log.java
+++ src/main/java/org/example/Log.java
@@ -3,4 +3,9 @@ package org.example;
 import java.time.LocalDateTime;
 
 public class Log implements Message {
+
+    public void affichermessage(String S) {
+        System.out.println(LocalDateTime.now()+S);
+    }
+
 }
